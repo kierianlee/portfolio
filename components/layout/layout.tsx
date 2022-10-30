88cd264d1ddf311902b0e5f6fc2a277980a381f0
Kierien <kierien@gmail.com>
2022-10-31 02:56:57 +0800

    layout fix

--- components/layout/layout.tsx
+++ components/layout/layout.tsx
@@ -39,59 +39,61 @@ const Layout = ({ children }: { children: ReactNode }) => {
         <CodeTag className="ml-4" tag="/body" />
         <CodeTag tag="/html" className="mt-4" />
       </div>
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{
-          opacity: 1,
-          transition: {
-            delay: theme.dirty ? 0 : 1.8,
-            duration: 1,
-          },
-        }}
-        className="absolute left-1/2 z-20 flex h-10 w-full -translate-x-1/2 items-center justify-center bg-gray-100 py-2 px-6 dark:bg-phantom-dark sm:justify-between"
-      >
-        <div className="hidden sm:block" />
-        <div className="flex flex-wrap items-center gap-x-8 gap-y-2">
-          {navLinks.map(({ label, path }, index) => (
-            <Link
-              href={path}
-              className={`relative font-mono text-xs font-light md:text-sm ${
-                (router.pathname.includes(path) && path !== "/") ||
-                (path === "/" && router.pathname === "/")
-                  ? "text-blue-500 dark:text-crayola"
-                  : "text-muted dark:text-[rgba(255,255,255,0.4)]"
-              } duration-100 hover:text-black dark:hover:text-white ${
-                index === navLinks.length - 1
-                  ? ""
-                  : `after:pointer-events-none after:absolute after:left-[calc(100%+1rem)] after:-translate-x-1/2 after:text-muted after:content-['/'] dark:after:text-[rgba(255,255,255,0.4)]`
-              } `}
-              key={index}
-            >
-              {label}
-            </Link>
-          ))}
-        </div>
-        <Switch
-          checked={theme.darkMode}
-          onChange={() => theme.toggleThemeHandler()}
-          className={`${
-            theme.darkMode ? "bg-[rgba(8,253,216,0.7)]" : "bg-gray-400"
-          } relative hidden h-6 w-11 items-center rounded-full sm:inline-flex`}
+      {theme.ready && (
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{
+            opacity: 1,
+            transition: {
+              delay: theme.dirty ? 0 : 1.8,
+              duration: 1,
+            },
+          }}
+          className="absolute left-1/2 z-20 flex h-10 w-full -translate-x-1/2 items-center justify-center bg-gray-100 py-2 px-6 dark:bg-phantom-dark sm:justify-between"
         >
-          <span className="sr-only">Dark mode</span>
-          <span className="absolute left-1">
-            <FontAwesomeIcon icon={faMoon} color="#F2C315" />
-          </span>
-          <span className="absolute right-1">
-            <FontAwesomeIcon icon={faSun} color="#F2C315" />
-          </span>
-          <span
+          <div className="hidden sm:block" />
+          <div className="flex flex-wrap items-center gap-x-8 gap-y-2">
+            {navLinks.map(({ label, path }, index) => (
+              <Link
+                href={path}
+                className={`relative font-mono text-xs font-light md:text-sm ${
+                  (router.pathname.includes(path) && path !== "/") ||
+                  (path === "/" && router.pathname === "/")
+                    ? "text-blue-500 dark:text-crayola"
+                    : "text-muted dark:text-[rgba(255,255,255,0.4)]"
+                } duration-100 hover:text-black dark:hover:text-white ${
+                  index === navLinks.length - 1
+                    ? ""
+                    : `after:pointer-events-none after:absolute after:left-[calc(100%+1rem)] after:-translate-x-1/2 after:text-muted after:content-['/'] dark:after:text-[rgba(255,255,255,0.4)]`
+                } `}
+                key={index}
+              >
+                {label}
+              </Link>
+            ))}
+          </div>
+          <Switch
+            checked={theme.darkMode}
+            onChange={() => theme.toggleThemeHandler()}
             className={`${
-              theme.darkMode ? "translate-x-6" : "translate-x-1"
-            } inline-block h-4 w-4 transform rounded-full bg-white transition`}
-          />
-        </Switch>
-      </motion.div>
+              theme.darkMode ? "bg-[rgba(8,253,216,0.7)]" : "bg-gray-400"
+            } relative hidden h-6 w-11 items-center rounded-full sm:inline-flex`}
+          >
+            <span className="sr-only">Dark mode</span>
+            <span className="absolute left-1">
+              <FontAwesomeIcon icon={faMoon} color="#F2C315" />
+            </span>
+            <span className="absolute right-1">
+              <FontAwesomeIcon icon={faSun} color="#F2C315" />
+            </span>
+            <span
+              className={`${
+                theme.darkMode ? "translate-x-6" : "translate-x-1"
+              } inline-block h-4 w-4 transform rounded-full bg-white transition`}
+            />
+          </Switch>
+        </motion.div>
+      )}
       <div className="fixed bottom-12 right-12 z-30 sm:hidden">
         {theme.darkMode ? (
           <div
