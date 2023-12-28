74a06ce0233448980e423ec238cce00f4085589d
Kierien <kierien@gmail.com>
2023-12-29 05:51:18 +0800

    feat: posthog integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,15 +6,32 @@ import { NextPage } from "next";
 import { ThemeContextProvider } from "../contexts/theme";
 import { DefaultSeo } from "next-seo";
 
+import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
+import { useRouter } from "next/router";
+
 export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
 };
+
+// Check that PostHog is client-side (used to handle Next.js SSR)
+if (typeof window !== "undefined") {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY || "", {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || "https://app.posthog.com",
+    // Enable debug mode in development
+    loaded: (posthog) => {
+      if (process.env.NODE_ENV === "development") posthog.debug();
+    },
+  });
+}
 
 type AppPropsWithLayout = AppProps & {
   Component: NextPageWithLayout;
 };
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+  const router = useRouter();
+
   const getLayout = Component.getLayout || ((page) => page);
 
   useEffect(() => {
@@ -26,13 +43,23 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
     }
   }, []);
 
+  useEffect(() => {
+    // Track page views
+    const handleRouteChange = () => posthog?.capture("$pageview");
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, []);
+
   return (
-    <>
+    <PostHogProvider client={posthog}>
       <DefaultSeo {...seoConfig} />
       <ThemeContextProvider>
         {getLayout(<Component {...pageProps} />)}
       </ThemeContextProvider>
-    </>
+    </PostHogProvider>
   );
 }
 
